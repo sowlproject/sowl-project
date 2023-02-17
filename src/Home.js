64d68f9da427b3798444c22f70c91bd6170a3219
junseokmac <wicaduce@gmail.com>
2023-02-17 16:52:33 +0900

    fix Home.scss

--- src/Home.js
+++ src/Home.js
@@ -12,7 +12,7 @@ export default function Home() {
                 <img src={Home_camera_icon} className="Home_camera_icon" alt="camera_icon"></img>
                 <div className="btn_block"><Link to="/frame" className="StartButton"><h5>촬영 시작하기</h5></Link></div>
                 <h3 className="kor_subtext">촬영 시작 버튼을 눌러 소울네컷 촬영을 시작합니다.</h3>
-                <h4 className="eng_subtext">Press the start button to shooting the s.owl 4 cut.</h4>
+                <h4 className="eng_subtext">Press the button to start the s.owl 4 cut.</h4>
                 <img src={Home_frame40} className="Home_frame40" alt="Home_frame"></img>
                 <img src={Home_frame60} className="Home_frame60" alt="Home_frame"></img>
             </div>
