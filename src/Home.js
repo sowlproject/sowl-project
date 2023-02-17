d2cfb8cf719ffa4a7add4dea0fd813dab6942166
junseokmac <wicaduce@gmail.com>
2023-02-17 12:12:46 +0900

    add Home_frame40,60
    use position: fixed;

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.scss";
 import Home_camera_icon from "./img/Home_camera_icon.png";
+import Home_frame40 from "./img/Home-frame40.png";
+import Home_frame60 from "./img/Home-frame60.png";
 
 export default function Home() {
     return (
@@ -11,6 +13,7 @@ export default function Home() {
                 <div className="btn_block"><Link to="/frame" className="StartButton"><h5>촬영 시작하기</h5></Link></div>
                 <h3 className="kor_subtext">촬영 시작 버튼을 눌러 소울네컷 촬영을 시작합니다.</h3>
                 <h4 className="eng_subtext">Press the start button to shooting the s.owl 4 cut.</h4>
+                <img src={Home_frame40} className="Home_frame40" alt="Home_frame"></img>
             </div>
     );
 }
