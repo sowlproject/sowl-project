5638c3307e1af539781ff74e1dbbbfec6ee8eb1f
junseokmac <wicaduce@gmail.com>
2023-02-18 11:01:08 +0900

    add credit_text
    본 서비스는 개발 동아리 S.owl에서 제작하였습니다. 개발 : 최준혁, 황재현, 윤준석 / 프레임 디자인: 강지수

--- src/Home.js
+++ src/Home.js
@@ -20,6 +20,7 @@ export default function Home() {
                 <div className="btn_block"><Link to="/frame" className="StartButton"><h5>촬영 시작하기</h5></Link></div>
                 <h3 className="kor_subtext">촬영 시작 버튼을 눌러 소울네컷 촬영을 시작합니다.</h3>
                 <h4 className="eng_subtext">Press the button to start the s.owl 4 cut.</h4>
+                <h4 className="credit_text">본 서비스는 개발 동아리 S.owl에서 제작하였습니다. 개발 : 최준혁, 황재현, 윤준석 / 프레임 디자인: 강지수</h4>
                 {/* <img src={Home_frame40} className="Home_frame40" alt="Home_frame"></img> */}
                 {/* <img src={Home_frame60} className="Home_frame60" alt="Home_frame"></img> */}
             </div>
